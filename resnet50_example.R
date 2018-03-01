e561ca46651f74dbd1c13a21de0062a2d5c6b95d
Jasmine Dumas <jasdumas@users.noreply.github.com>
2018-03-01 09:21:29 -0800

    Add files via upload

---
+++ resnet50_example.R
@@ -0,0 +1,22 @@
+# # # # # # # # # # # # # # # # # # # # 
+#  Classify Zoo Animal Image with Keras 
+# # # # # # # # # # # # # # # # # # # #
+library(keras)
+# install_keras()
+
+# instantiate the model
+model <- application_resnet50(weights = 'imagenet')
+
+# load the image (copy an image from Photos)
+img_path <- "butterfly.jpg"
+img <- image_load(img_path, target_size = c(224,224))
+x <- image_to_array(img)
+
+# ensure we have a 4d tensor with single element in the batch dimension,
+# the preprocess the input for prediction using resnet50
+x <- array_reshape(x, c(1, dim(x)))
+x <- imagenet_preprocess_input(x)
+
+# make predictions then decode and print them
+preds <- model %>% predict(x)
+imagenet_decode_predictions(preds, top = 3)[[1]]
